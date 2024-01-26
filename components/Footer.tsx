c990915c9448399c5c5f10fc5dfef02ee06561b7
felidz <24916531+felidz@users.noreply.github.com>
2024-01-26 11:57:25 +0900

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,6 +20,7 @@ import styles from './styles.module.css'
 export const FooterImpl: React.FC = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
+  const startYear = 2019
   const currentYear = new Date().getFullYear()
 
   const onToggleDarkMode = React.useCallback(
@@ -36,7 +37,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
+      <div className={styles.copyright}>© {config.author} {startYear}-{currentYear} {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
